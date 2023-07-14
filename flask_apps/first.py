7cffed2a8ec58a5e0ec94046bc0e5dadd7ced135
illumi-410 <sandeepparajuli96@gmail.com>
2023-07-14 12:38:46 +0545

    count

--- flask_apps/first.py
+++ flask_apps/first.py
@@ -1,6 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, session
+from redis import Redis
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
+
+redis = Redis()
 
 @app.route("/")
 def hello_world():
@@ -8,4 +12,16 @@ def hello_world():
     if arg:
         return f'<h2>Hello, {arg}!</h2>'
     else:
-        return f'<h2>Hello, World!</h2>'
+        return f'''
+                <h2>Hello, World!</h2>
+                <a href='/count'>Count Page</a>
+        '''
+    
+@app.route('/count')
+def count():
+    session['visit_count'] = session.get('visit_count', 0) + 1
+    return f'''
+            Total visit: {session["visit_count"]}
+            <a href='/'>Home</a>
+    '''
+
