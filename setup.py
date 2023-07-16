3120ba78e0923443328d1516ad5bf7b2381a41ca
vboxuser <sandeep@Ubuntu.myguest.virtualbox.org>
2023-07-16 12:17:53 +0545

    working

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name = 'flask_apps',
-    version = '1.0.0',
-    description = 'Flask first project',
-    author='Sandeep Parajuli',
-    packages=['flask_apps'],
-)
